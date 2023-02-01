77421731b1dbd574656427f8c106a29e0b480ab9
medinahasna <medinahasna69@gmail.com>
2023-02-02 00:00:21 +0700

    Commit -m "I add print kaedehara kazuha"

---
+++ PythonWorld/anemoboys.py
@@ -0,0 +1,14 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> sixbreeze_dict = {
+...     'Kaedehara_Kazuha': 'Inazuman Anemo Sword',
+...     'Shikanoin_Heizou': 'Inazuman Anemo Catalyst',
+...     'Xiao': 'Liyue Anemo Polearm',
+...     'Venti': 'Mondstadtian Anemo Bow',
+...     'Wanderer_Scaramouche': 'Sumeru Anemo Catalyst',
+...     'Aether_Traveler': 'Outlander Anemo Sword'
+...     }
+>>> print(sixbreeze_dict)
+{'Kaedehara_Kazuha': 'Inazuman Anemo Sword', 'Shikanoin_Heizou': 'Inazuman Anemo Catalyst', 'Xiao': 'Liyue Anemo Polearm', 'Venti': 'Mondstadtian Anemo Bow', 'Wanderer_Scaramouche': 'Sumeru Anemo Catalyst', 'Aether_Traveler': 'Outlander Anemo Sword'}
+>>> print(sixbreeze_dict['Wanderer_Scaramouche'])
+Sumeru Anemo Catalyst
